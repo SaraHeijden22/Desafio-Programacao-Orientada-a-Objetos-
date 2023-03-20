3909e17ce6045ed79948ca8c5825e5fc2413fff9
Sara Lopes <sarah.jcs2014@gmail.com>
2023-03-20 14:04:45 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -9,13 +8,13 @@ public class Main {
 
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
-        curso1.setDescrição("descrição do curso java");
+        curso1.setDescricao("descrição do curso java");
         curso1.setCargaHoraria(8);
 
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
-        curso2.setDescrição("descrição do curso js");
+        curso2.setDescricao("descrição do curso js");
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
@@ -24,9 +23,35 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
+        Bootcamp bootcamp =  new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+
+        Dev devSara = new Dev();
+        devSara.setNome("Sara");
+        devSara.inscreverBootcamp(bootcamp);
+        System.out.println(" Conteudos Inscritos " + devSara.getConteudosInscritos());
+        devSara.progredir();
+        System.out.println(" Conteudos Inscritos " + devSara.getConteudosInscritos());
+        System.out.println("Conteudos Inscritos " + devSara.getConteudosConcluidos());
+
+
+        System.out.println("----------------------------");
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devSara.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteudos Inscritos " + devCamila.getConteudosConcluidos());
+
+
+
+
+
+
     }
 }
